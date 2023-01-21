ef4daba8e127fe636b46cd256fcca813ca404bef
Ashwani Kumar <ashwani1609200@gmail.com>
2023-01-21 14:03:55 +0530

    sd

--- 4.Advanced ReactJs/pininput/src/components/PinInput.js
+++ 4.Advanced ReactJs/pininput/src/components/PinInput.js
@@ -3,15 +3,32 @@ import "./PinInput.mudule.css"
 export const PinInput = ({length,size}) => {
     const [arr]=useState(new Array(length).fill(""))
     const ref=useRef([])
-    console.log(ref)
+    const [text,setText]=useState("")
+
+const next=(i)=>{
+        ref.current[i].focus()
+}    
 const changeHandler=(e,i)=>{
+    if(i<length-1){
+        arr[i]=e.key
+        next(i+1)
+    }
+    if(i>=0 && e.keyCode==8){
+        next(i-1)
+    }
+   
+    setText(arr.join(""))
 
+    
+  
 }
-
+const copyHandler=(e)=>{
+    console.log(e.target.value)
+}
   return <div>PinInput<br/>
 {
     arr.map((e,i)=>{
-       return <input onChange={changeHandler} maxLength={size} key={i} />
+       return <input ref={(e)=>ref.current[i]=e} onPaste={(e)=>{copyHandler(e)}} key={i} onKeyUp={(e)=>{changeHandler(e,i)}} maxLength={size}  />
     })
 }
 
