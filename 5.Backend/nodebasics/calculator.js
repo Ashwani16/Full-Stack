07ad06c4fed1643474a99ca3a6a5bcc086445483
Ashwani Kumar <ashwani1609200@gmail.com>
2023-01-28 12:34:10 +0530

    jn

--- 5.Backend/nodebasics/calculator.js
+++ 5.Backend/nodebasics/calculator.js
@@ -1,1 +1,36 @@
-console.table([{ a: 1, b: 'Y' }, { a: 'Z', b: 2 }]);
+//console.table([{ a: 1, b: 'Y' }, { a: 'Z', b: 2 }]);
+const argument=process.argv
+console.log(argument)
+switch(argument[2]){
+    case("add"):{
+        console.log(+argument[3]+(+argument[4]))
+        break;
+    }
+    case("sub"):{
+        console.log(+argument[3]-(+argument[4]))
+        break;
+    }
+    case("mult"):{
+        console.log(+argument[3]*(+argument[4]))
+        break;
+    }
+    case("divide"):{
+        console.log(+argument[3]/(+argument[4]))
+        break;
+    }
+    case("sin"):{
+        console.log(Math.sin(+argument[3]))
+        break;
+    }
+    case("cos"):{
+        console.log(Math.cos(+argument[3]))
+        break;
+    }case("random"):{
+        console.log(Math.random())
+        break;
+    }
+    default:{
+        console.log("enter valid syntax")
+        break;
+    }
+}
