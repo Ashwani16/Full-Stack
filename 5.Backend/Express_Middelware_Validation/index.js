c919a4d37f0ef84f679804dfa408e7db4cc44cae
Ashwani Kumar <ashwani1609200@gmail.com>
2023-02-02 10:28:33 +0530

    created validTION middle ware that validate that incoming data have own property or not andits value are write or not

--- 5.Backend/Express_Middelware_Validation/index.js
+++ 5.Backend/Express_Middelware_Validation/index.js
@@ -1,10 +1,17 @@
 const express=require("express");
 const app=express();
-
+app.use(express.json())
 app.use((req,res,next)=>{
+    let data=req.body;
+    if(typeof data.id ==="number"&&typeof data.name==="string" &&typeof data.rating ==="number"&&typeof data.desc==="string"&&typeof data.genre==="string"&& Array.isArray(data.cast)){
+        next();
+    }
+    else{
+        res.sendStatus(400);
+    }
     
 })
-app.post("./movies",(req,res)=>{
+app.post("/movies",(req,res)=>{
     res.end("data recieved successfully")
 })
 
